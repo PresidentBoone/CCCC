8c039916a50ce38b2d7bf2f37922491adb840831
PresidentBoone <dylonboone@gmail.com>
2025-07-20 22:53:32 -0500

    auth

---
+++ firebas-config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDqL5ZoTKp36sk8J5TxuHn_y6ji4i9h20s",
+  authDomain: "collegeclimb-ai.firebaseapp.com",
+  projectId: "collegeclimb-ai",
+  storageBucket: "collegeclimb-ai.firebasestorage.app",
+  messagingSenderId: "187139654658",
+  appId: "1:187139654658:web:4a6cf4c43095f03212931b",
+  measurementId: "G-E0B2RQM9XS"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
